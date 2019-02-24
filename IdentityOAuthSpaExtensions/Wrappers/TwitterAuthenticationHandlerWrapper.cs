ce7a04a28f3646fdfa884229ad2dcfced32cbf3a
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2019-02-24 22:43:56 +0700

    Twitter made working

---
+++ IdentityOAuthSpaExtensions/Wrappers/TwitterAuthenticationHandlerWrapper.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.Twitter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json;
+
+namespace IdentityOAuthSpaExtensions.Wrappers
+{
+    public class TwitterAuthenticationHandlerWrapper : RemoteAuthenticationHandlerWrapper<TwitterOptions>
+    {
+        public class CodeContainer
+        {
+            public string OAuthToken { get; set; }
+            public string OAuthVerifier { get; set; }
+        }
+        private readonly TwitterHandler _authHandler;
+
+        public TwitterAuthenticationHandlerWrapper(TwitterHandler authHandler,
+            IHttpContextAccessor httpContextAccessor) : base(authHandler, httpContextAccessor)
+        {
+            _authHandler = authHandler;
+        }
+        
+        public override AuthenticationProperties Unprotect(string state)
+        {
+            return Options.StateDataFormat.Unprotect(state).Properties;
+        }
+
+        protected override void StubRequest(string code, string absoluteCallbackUrl)
+        {
+            var deserializedToken = JsonConvert.DeserializeObject<CodeContainer>(code);
+            
+            var context = new DefaultHttpContext();
+            var request = (DefaultHttpRequest) context.Request;
+            var authenticationProperties = new AuthenticationProperties(new Dictionary<string, string>()
+            {
+                {OpenIdConnectDefaults.RedirectUriForCodePropertiesKey, absoluteCallbackUrl}
+            });
+            
+            var state = new Microsoft.AspNetCore.Authentication.Twitter.RequestToken()
+            {
+                Token = deserializedToken.OAuthToken,
+                TokenSecret = deserializedToken.OAuthVerifier,
+                CallbackConfirmed = true,
+                Properties = authenticationProperties,
+            };
+            request.Cookies = new RequestCookieCollection(new Dictionary<string, string>()
+            {
+                {Options.StateCookie.Name, Options.StateDataFormat.Protect(state)}
+            });
+            request.Query = new QueryCollection(new Dictionary<string, StringValues>()
+            {
+                {"oauth_token", deserializedToken.OAuthToken},
+                {"oauth_verifier", deserializedToken.OAuthVerifier},
+            });
+//            request.Form = new FormCollection(new Dictionary<string, StringValues>()
+//            {
+//                {"state", Options.StateDataFormat.Protect(state)},
+//            });
+            _authHandler.SetHttpContext(context);
+        }
+    }
+}
