85f3dd0f6ce34ff8733f85ae687e7503150bf166
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2019-02-26 01:05:25 +0700

    https redirect on dev only

--- IdentityOAuthSpaExtensions.Example/Startup.cs
+++ IdentityOAuthSpaExtensions.Example/Startup.cs
@@ -98,14 +98,14 @@ namespace IdentityOAuthSpaExtensions.Example
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseHttpsRedirection();
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseIdentityServer();
             app.UseAuthentication();
 
